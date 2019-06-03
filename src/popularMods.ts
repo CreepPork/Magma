4e81b06423178f6939600b7e249839034acc8051
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-06-03 13:42:01 +0300

    Filled out popular mod list

--- src/popularMods.ts
+++ src/popularMods.ts
@@ -1,19 +1,59 @@
 export const popularMods: IMod[] = [
     {
-        id: 0,
-        name: 'ACE3',
+        id: 463939057,
+        name: 'ace',
     },
     {
-        id: 1,
+        id: 723217262,
         name: 'Achilles',
     },
     {
-        id: 2,
-        name: 'Advanced Rappeling',
+        id: 713709341,
+        name: 'Advanced Rappelling',
     },
     {
-        id: 3,
-        name: 'CBA A3',
+        id: 450814997,
+        name: 'CBA_A3',
+    },
+    {
+        id: 583496184,
+        name: 'CUP Terrains - Core',
+    },
+    {
+        id: 583544987,
+        name: 'CUP Terrains - Maps',
+    },
+    {
+        id: 497661914,
+        name: 'CUP Units',
+    },
+    {
+        id: 541888371,
+        name: 'CUP Vehicles',
+    },
+    {
+        id: 497660133,
+        name: 'CUP Weapons',
+    },
+    {
+        id: 843425103,
+        name: 'RHSAFRF',
+    },
+    {
+        id: 843593391,
+        name: 'RHSGREF',
+    },
+    {
+        id: 843632231,
+        name: 'RHSSAF',
+    },
+    {
+        id: 843577117,
+        name: 'RHSUSAF',
+    },
+    {
+        id: 620019431,
+        name: 'task_force_radio',
     },
 ];
 
