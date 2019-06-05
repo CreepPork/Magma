a1285c727e662e58e0aaaa9c8f2f83d7998d6282
Ralfs <creepporkstudios@gmail.com>
2019-06-05 22:38:46 +0300

    Added update command

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -3,7 +3,11 @@ import Settings from '../settings';
 import Download from './download';
 
 export default class Install extends Command {
-    public static description = 'Installs all mods that are present in your configuration file.';
+    public static description = 'Installs and updates all mods that are present in your configuration file.';
+
+    public static aliases = [
+        'update',
+    ];
 
     public async run() {
         this.checkIfInitialized();
