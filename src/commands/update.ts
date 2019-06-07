52cc3bf1a54171df5474fec20e6e6e292247f358
Ralfs Garkaklis <creepporkstudios@gmail.com>
2019-06-07 21:21:54 +0300

    Await Download Run and fixed desc

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -3,13 +3,13 @@ import Settings from '../settings';
 import Download from './download';
 
 export default class Update extends Command {
-    public static description = 'Updates installed mods to their latest versions.';
+    public static description = 'Updates installed mods and keys to their latest versions.';
 
     public async run() {
         this.checkIfInitialized();
 
         const mods = Settings.get('mods');
 
-        Download.run(mods.map(mod => mod.itemId.toString()));
+        await Download.run(mods.map(mod => mod.itemId.toString()));
     }
 }
