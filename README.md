6b926c4364d60c3e5fba91c92ab021e7898b1aae
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-12 23:12:49 +0300

    Added fully functional encrypt/decrypt library

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ A CLI app that is a mod updater for Arma 3 servers.
 # Usage
 <!-- usage -->
 ```sh-session
-$ npm install -g magma
+$ npm install -g @creeppork/magma
 $ magma COMMAND
 running command...
 $ magma (-v|--version|version)
-magma/2.0.0 linux-x64 node-v10.16.0
+@creeppork/magma/2.0.0 linux-x64 node-v10.16.0
 $ magma --help [COMMAND]
 USAGE
   $ magma COMMAND
@@ -29,7 +29,42 @@ USAGE
 <!-- usagestop -->
 # Commands
 <!-- commands -->
+* [`magma add`](#magma-add)
+* [`magma cron`](#magma-cron)
+* [`magma download`](#magma-download)
 * [`magma help [COMMAND]`](#magma-help-command)
+* [`magma initialize`](#magma-initialize)
+* [`magma install`](#magma-install)
+* [`magma login`](#magma-login)
+* [`magma remove`](#magma-remove)
+* [`magma update`](#magma-update)
+
+## `magma add`
+
+```
+USAGE
+  $ magma add
+```
+
+_See code: [src/commands/add.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/add.ts)_
+
+## `magma cron`
+
+```
+USAGE
+  $ magma cron
+```
+
+_See code: [src/commands/cron.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/cron.ts)_
+
+## `magma download`
+
+```
+USAGE
+  $ magma download
+```
+
+_See code: [src/commands/download.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/download.ts)_
 
 ## `magma help [COMMAND]`
 
@@ -47,4 +82,49 @@ OPTIONS
 ```
 
 _See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v2.2.1/src/commands/help.ts)_
+
+## `magma initialize`
+
+```
+USAGE
+  $ magma initialize
+```
+
+_See code: [src/commands/initialize.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/initialize.ts)_
+
+## `magma install`
+
+```
+USAGE
+  $ magma install
+```
+
+_See code: [src/commands/install.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/install.ts)_
+
+## `magma login`
+
+```
+USAGE
+  $ magma login
+```
+
+_See code: [src/commands/login.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/login.ts)_
+
+## `magma remove`
+
+```
+USAGE
+  $ magma remove
+```
+
+_See code: [src/commands/remove.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/remove.ts)_
+
+## `magma update`
+
+```
+USAGE
+  $ magma update
+```
+
+_See code: [src/commands/update.ts](https://github.com/CreepPork/Magma/blob/v2.0.0/src/commands/update.ts)_
 <!-- commandsstop -->
---
+++ src/encrypter.ts
@@ -0,0 +1,164 @@
+import * as Crypto from 'crypto';
+
+import IEncryptedPayload from './interfaces/iEncryptedPayload';
+
+/**
+ * Based on Laravel's implementation.
+ */
+export default class Encrypter {
+    /**
+     * Default cipher to be used is AES-256-CBC.
+     */
+    public static readonly DEFAULT_CIPHER = 'AES-256-CBC';
+
+    /**
+     * Determine if the given key and cipher combination is valid.
+     */
+    public static supported(key: string, cipher: string): boolean {
+        const length = Buffer.byteLength(key, 'base64');
+
+        return (cipher === 'AES-128-CBC' && length === 16) ||
+            (cipher === 'AES-256-CBC' && length === 32);
+    }
+
+    /**
+     * Create a new encryption key for the given cipher.
+     */
+    public static generateKey(cipher: string): string {
+        return Crypto.randomBytes(cipher === 'AES-128-CBC' ? 16 : 32).toString('base64');
+    }
+
+    /**
+     * For AES it is always 16.
+     */
+    private static readonly IV_LENGTH = 16;
+
+    /**
+     * The encryption key.
+     */
+    private key: string;
+
+    /**
+     * The algorithm used for encryption.
+     */
+    private cipher: string;
+
+    /**
+     * Create a new encrypter instance.
+     */
+    constructor(key: string, cipher = Encrypter.DEFAULT_CIPHER) {
+        if (Encrypter.supported(key, cipher)) {
+            this.key = key;
+            this.cipher = cipher;
+        } else {
+            throw new Error('The only supported ciphers are AES-128-CBC and AES-256-CBC with the correct key lengths.');
+        }
+    }
+
+    /**
+     * Encrypt the given value.
+     */
+    public encrypt(data: any, stringify = true): string {
+        const iv = Crypto.randomBytes(Encrypter.IV_LENGTH);
+
+        /**
+         * First we will encrypt the value using OpenSSL. After this is encrypted we
+         * will proceed to calculating a MAC for the encrypted value so that this
+         * value can be verified later as not having been changed by the users.
+         */
+        const cipher = Crypto.createCipheriv(this.cipher, Buffer.from(this.key, 'base64'), iv);
+        let encrypted = cipher.update(stringify ? JSON.stringify(data) : data);
+
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+        /**
+         * Once we get the encrypted value we'll go ahead and base64_encode the input
+         * vector and create the MAC for the encrypted value so we can then verify
+         * its authenticity. Then, we'll JSON the data into the "payload" array.
+         */
+        const mac = this.hash(iv.toString(), encrypted.toString());
+
+        const payload: IEncryptedPayload = { iv, encrypted, mac };
+        const json = JSON.stringify(payload);
+
+        return Buffer.from(json).toString('base64');
+    }
+
+    /**
+     * Decrypt the given value.
+     */
+    public decrypt(payload: string, unserialize = true): string {
+        const json = this.getJsonPayload(payload);
+
+        const iv = Buffer.from(json.iv);
+
+        const decipher = Crypto.createDecipheriv(this.cipher, Buffer.from(this.key, 'base64'), iv);
+
+        const decryptedBuffer = decipher.update(Buffer.from(json.encrypted));
+        const decrypted = Buffer.concat([decryptedBuffer, decipher.final()]).toString();
+
+        return unserialize ? JSON.parse(decrypted) : decrypted;
+    }
+
+    /**
+     * Create a MAC for the given value.
+     */
+    private hash(iv: string, value: string): string {
+        return Crypto.createHmac('sha256', this.key).update(iv + value).digest('hex');
+    }
+
+    /**
+     * Get the JSON array from the given payload.
+     */
+    private getJsonPayload(payload: string): IEncryptedPayload {
+        const json = JSON.parse(Buffer.from(payload, 'base64').toString());
+
+        if (! this.validPayload(json)) {
+            throw new Error('The payload is invalid.');
+        }
+
+        if (! this.validMac(json)) {
+            throw new Error('The MAC is invalid.');
+        }
+
+        return json;
+    }
+
+    /**
+     * Verify that the encryption payload is valid.
+     */
+    private validPayload(payload: IEncryptedPayload | any): boolean {
+        return typeof payload === 'object' && payload.iv && payload.encrypted && payload.mac &&
+            Buffer.from(payload.iv, 'base64').length === Encrypter.IV_LENGTH;
+    }
+
+    /**
+     * Determine if the MAC for the given payload is valid.
+     */
+    private validMac(payload: IEncryptedPayload): boolean {
+        const bytes = Crypto.randomBytes(Encrypter.IV_LENGTH);
+
+        const comparision = Crypto.createHmac(
+            'sha256',
+            bytes,
+        ).update(payload.mac).digest('hex');
+
+        const calculated = this.calculateMac(payload, bytes);
+
+        return Crypto.timingSafeEqual(
+            Buffer.from(comparision),
+            Buffer.from(calculated),
+        );
+    }
+    /**
+     * Calculate the hash of the given payload.
+     */
+    private calculateMac(payload: IEncryptedPayload, bytes: Buffer): string {
+        return Crypto.createHmac(
+            'sha256',
+            bytes,
+        ).update(this.hash(
+            Buffer.from(payload.iv as any, 'base64').toString(), Buffer.from(payload.encrypted).toString(),
+        )).digest('hex');
+    }
+}
