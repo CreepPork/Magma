cc2fad41acf9eac7be214aef77dee85bf19b90d2
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-13 23:04:55 +0300

    Added config

--- test/tests/config.test.ts
+++ test/tests/config.test.ts
@@ -1,6 +1,58 @@
 import * as fs from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
 
 import Config from '../../src/config';
+
+describe('Config.get()', () => {
+    test('Returns the correct key from JSON', () => {
+        const orgEnsure = Config.ensureIsInitialized;
+        const orgAll = Config.getAll;
+
+        Config.ensureIsInitialized = jest.fn().mockReturnValue(true);
+
+        Config.getAll = jest.fn().mockReturnValue({
+            key: 'someString',
+            mods: [],
+        });
+
+        expect(Config.get('key')).toBe('someString');
+
+        Config.ensureIsInitialized = orgEnsure;
+        Config.getAll = orgAll;
+    });
+});
+
+describe('Config.set()', () => {
+    test('Data is written correctly', () => {
+        // @ts-ignore Private method
+        const orgPath = Config.path;
+
+        const file = path.join(os.tmpdir(), 'exampleMagma.json');
+
+        // @ts-ignore Private method
+        Config.path = jest.fn().mockReturnValue(file);
+
+        fs.writeFileSync(file, JSON.stringify({
+            key: 'exampleTest',
+            serverPath: 'some/path',
+        }));
+
+        Config.set('key', 'otherKey');
+
+        expect(JSON.parse(fs.readFileSync(file).toString())).toStrictEqual({
+            key: 'otherKey',
+            serverPath: 'some/path',
+        });
+
+        if (fs.existsSync(file)) {
+            fs.removeSync(file);
+        }
+
+        // @ts-ignore Private method
+        Config.path = orgPath;
+    });
+});
 
 describe('Config.ensureIsInitialized()', () => {
     test('Error is thrown if magma.json does not exist', () => {
