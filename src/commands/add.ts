d3eb3779cdb0f032c494ad7894347cc2d96f04d0
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-18 17:48:37 +0300

    Work in progress add command

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,12 +1,32 @@
 import Command from '@oclif/command';
+import { IArg } from '@oclif/parser/lib/args';
 import Config from '../config';
 
+import ora = require('ora');
+import Mod from '../mod';
+
 export default class Add extends Command {
+    public static description = 'Adds Steam Workshop items to the configuration files (does not download them).';
+    public static examples = ['magma add 723217262', 'magma add 450814997 723217262 713709341'];
+    public static strict = false;
+    public static args = [{ description: 'Steam Workshop IDs.', name: 'ids', required: true }] as IArg[];
+
     public async init(): Promise<void> {
         Config.ensureIsInitialized();
     }
 
     public async run(): Promise<void> {
-        //
+        const { argv } = this.parse(Add);
+        const ids = argv.map(arg => parseInt(arg, 10));
+
+        // Get info from API
+        const mods = [];
+
+        for (const id of ids) {
+            // todo: think how to get type
+            mods.push(await Mod.getModsFromApi({ id, type }));
+        }
+
+        // Add to config
     }
 }
---
+++ src/mod.ts
@@ -0,0 +1,12 @@
+import { EModType } from './enums/eModType';
+import IMod from './interfaces/iMod';
+
+export default class Mod {
+    public static async getModFromApi(id: number, type: EModType): Promise<IMod> {
+        return (await this.getModsFromApi({ id, type }))[0];
+    }
+
+    public static async getModsFromApi(...mods: [{ id: number, type: EModType }]): Promise<IMod[]> {
+        //
+    }
+}
