6b20eaea0670057f7dd6ff37100972fcd2ad435e
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-19 21:05:49 +0300

    Add command works as intended now

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,35 +1,92 @@
-import Command from '@oclif/command';
+import Command, { flags as flag } from '@oclif/command';
 import { IArg } from '@oclif/parser/lib/args';
+import { prompt } from 'inquirer';
 
 import Config from '../config';
 import { EModType } from '../enums/eModType';
+import { nonInteractive } from '../flags';
 import Mod from '../mod';
+import SteamApi from '../steam/steamApi';
 
 import ora = require('ora');
 
 export default class AddCommand extends Command {
     public static description = 'Adds Steam Workshop items to the configuration files (does not download them).';
-    public static examples = ['magma add 723217262', 'magma add 450814997 723217262 713709341'];
+    public static examples = [
+        'magma add 723217262 --type client',
+        'magma add 450814997 723217262 713709341 --type all client server',
+    ];
     public static strict = false;
     public static args = [{ description: 'Steam Workshop item IDs.', name: 'id', required: true }] as IArg[];
+    public static flags = {
+        nonInteractive,
+        type: flag.string({
+            char: 't',
+            default: 'all',
+            multiple: true,
+            options: ['all', 'client', 'server'],
+        }),
+    };
 
     public async init(): Promise<void> {
         Config.ensureIsInitialized();
     }
 
     public async run(): Promise<void> {
-        const { argv } = this.parse(AddCommand);
+        const { argv, flags } = this.parse(AddCommand);
         const ids = argv.map(arg => parseInt(arg, 10));
+
+        const configMods = Config.get('mods');
+        const spinner = ora();
 
         // Get info from API
         const mods = [];
+        const types: (keyof typeof EModType)[] = flags.type as any;
 
-        for (const id of ids) {
-            // Todo: think how to get type
-            const type = EModType.forAll;
-            mods.push(await Mod.getModsFromApi({ id, type }));
+        for (const [index, id] of ids.entries()) {
+            let type: EModType | undefined = EModType[types[index]];
+
+            if (type === undefined) {
+                if (flags.nonInteractive) {
+                    throw new Error(`Mod (id: ${id}) was not given a type. Did you enter the type with --type?`);
+                }
+
+                type = await this.promptForType(id, spinner);
+            }
+
+            if (configMods.find(mod => mod.id === id) === undefined) {
+                mods.push({ id, type });
+            }
         }
 
-        // Add to config
+        if (mods.length === 0) {
+            console.log('The given mods have already been added to the configuration file.');
+        } else {
+            spinner.start('Fetching Steam API data and generating mod data');
+            const fetchedMods = await Mod.getModsFromApi(...mods);
+            spinner.succeed();
+
+            // Add to config
+            configMods.push(...fetchedMods);
+
+            Config.set('mods', configMods);
+        }
+    }
+
+    private async promptForType(id: number, spinner: ora.Ora): Promise<EModType> {
+        const choices = ['Required for all', 'Client-side only', 'Server-side only'];
+
+        spinner.start(`Fetching Steam API data for ${id}`);
+        const name = (await SteamApi.getPublishedItem(id)).title;
+        spinner.succeed();
+
+        const response: { type: string } = await prompt({
+            choices,
+            message: `What type of mod is ${name}?`,
+            name: 'type',
+            type: 'list',
+        });
+
+        return EModType[choices.indexOf(response.type)] as any;
     }
 }
