b178bec24277a6e963563492bbb44aec659f8199
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-19 21:05:09 +0300

    Mod.getModsFromApi works now correctly

--- src/mod.ts
+++ src/mod.ts
@@ -1,12 +1,29 @@
 import { EModType } from './enums/eModType';
 import IMod from './interfaces/iMod';
+import SteamApi from './steam/steamApi';
 
 export default class Mod {
     public static async getModFromApi(id: number, type: EModType): Promise<IMod> {
         return (await this.getModsFromApi({ id, type }))[0];
     }
 
-    public static async getModsFromApi(...mods: [{ id: number, type: EModType }]): Promise<IMod[]> {
-        return [];
+    public static async getModsFromApi(...mods: { id: number, type: EModType }[]): Promise<IMod[]> {
+        if (mods.length === 0) { return []; }
+
+        const processed: IMod[] = [];
+
+        const items = await SteamApi.getPublishedItems(...mods.map(mod => mod.id));
+
+        for (const [index, mod] of mods.entries()) {
+            const name = items[index].title;
+
+            processed.push({
+                id: mod.id,
+                name,
+                type: mod.type,
+            });
+        }
+
+        return processed;
     }
 }
