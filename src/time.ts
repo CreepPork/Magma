8782d332003db21e34258db2f2a5abf44aa965cd
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-22 17:13:06 +0300

    Fixed wrong reading of Steam's updatedAt epoch

--- src/time.ts
+++ src/time.ts
@@ -1,9 +1,9 @@
 export default class Time {
     public static toEpoch(date: Date): number {
-        return date.getTime();
+        return date.getTime() / 1000;
     }
 
     public static epochToDate(epoch: number): Date {
-        return new Date(epoch);
+        return new Date(epoch * 1000);
     }
 }
