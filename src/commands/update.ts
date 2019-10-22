2cb9206b82f7c7433ebdccfa85090e00e31d9e28
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-22 18:28:55 +0300

    Added update command functionality

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -1,7 +1,47 @@
 import Command from '@oclif/command';
+import Config from '../config';
+import Mod from '../mod';
+import Processor from '../processor';
+import SteamCmd from '../steam/steamCmd';
 
-export default class UpdateCommand extends Command {
+export default class ActivateCommand extends Command {
+    public async init(): Promise<void> {
+        Config.ensureIsInitialized();
+    }
+
     public async run(): Promise<void> {
-        //
+        // Filter for only installed mods
+        const configMods = Config.get('mods');
+        const mods = configMods.filter(mod => mod.updatedAt !== undefined);
+
+        if (mods.length === 0) { return; }
+
+        // Check for those mods that need updates from SteamAPI
+        const apiMods = await Mod.getModUpdatedAtFromApi(mods);
+
+        const queuedMods = [];
+
+        for (const [index, mod] of mods.entries()) {
+            if (apiMods[index].updatedAt !== mod.updatedAt) {
+                queuedMods.push(mod);
+            }
+        }
+
+        if (queuedMods.length === 0) { return; }
+
+        // Run SteamCmd
+        await SteamCmd.download(...queuedMods.map(mod => mod.id));
+
+        // Update keys
+        const updatedMods = Processor.updateKeys(queuedMods);
+
+        // Update magma.json
+        for (const mod of updatedMods) {
+            const index = configMods.findIndex(configMod => configMod.id === mod.id);
+
+            configMods[index] = mod;
+        }
+
+        Config.set('mods', configMods);
     }
 }
