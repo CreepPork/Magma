af09373072f0a621cccb64d98271ee69e598e89f
Ralfs Garkaklis <ralfs@garkaklis.com>
2019-10-22 17:10:12 +0300

    Added more methods for filesystem

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs-extra';
+import * as klaw from 'klaw-sync';
 import * as path from 'path';
 
 export default class Filesystem {
@@ -18,6 +19,10 @@ export default class Filesystem {
         return false;
     }
 
+    public static getFilename(filepath: string): string {
+        return path.parse(filepath).base;
+    }
+
     public static getFilenameNoExt(filepath: string): string {
         return path.parse(filepath).name;
     }
@@ -27,5 +32,38 @@ export default class Filesystem {
             .map(el => path.join(filepath, el))
             .filter(this.isFile)
             .some(el => el.includes(search));
+    }
+
+    public static findFilesWithExtension(filepath: string, extension: string): string[] {
+        return this.getAllFilesRecursively(filepath).filter(file => file.includes(extension));
+    }
+
+    public static getAllDirectoriesRecursively(filepath: string): string[] {
+        return klaw(filepath, { nofile: true }).map(item => item.path);
+    }
+
+    public static getAllFilesRecursively(filepath: string): string[] {
+        return klaw(filepath, { nodir: true }).map(item => item.path);
+    }
+
+    public static renameContentsToLowercase(filepath: string): void {
+        const dirs = this.getAllDirectoriesRecursively(filepath);
+
+        for (const [i, dir] of dirs.entries()) {
+            if (fs.existsSync(dir)) {
+                fs.renameSync(dir, path.join(dir, '..', this.getFilename(dir).toLowerCase()));
+            } else {
+                const newDirs = this.getAllDirectoriesRecursively(filepath);
+                const updatedDir = newDirs[i];
+
+                fs.renameSync(updatedDir, path.join(updatedDir, '..', this.getFilename(updatedDir).toLowerCase()));
+            }
+        }
+
+        const files = this.getAllFilesRecursively(filepath);
+
+        for (const file of files) {
+            fs.renameSync(file, path.join(file, '..', this.getFilename(file).toLowerCase()));
+        }
     }
 }
