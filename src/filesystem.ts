3f122e6d9d9cec77b2a1b5996c6a26e953afb749
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-02-01 17:02:37 +0200

    Added helper fileSizeForHumans method

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -66,4 +66,18 @@ export default class Filesystem {
             fs.renameSync(file, path.join(file, '..', this.getFilename(file).toLowerCase()));
         }
     }
+
+    public static fileSizeForHumans(bytes: number, decimals = 2): string {
+        if (bytes === 0) {
+            return '0 Bytes';
+        };
+
+        const k = 1024;
+        const dm = decimals < 0 ? 0 : decimals;
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    }
 }
