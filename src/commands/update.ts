fbcd873e4629aee2b4bab875e5521870eebc15a6
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-02-02 18:39:50 +0200

    Added more output for update command

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -16,7 +16,11 @@ export default class ActivateCommand extends Command {
         const configMods = Config.get('mods');
         const mods = configMods.filter(mod => mod.updatedAt !== undefined);
 
-        if (mods.length === 0) { return; }
+        if (mods.length === 0) {
+            console.log('No installed mods.');
+
+            return;
+        }
 
         // Check for those mods that need updates from SteamAPI
         const apiMods = await SteamApi.getPublishedItems(...mods.map(mod => mod.id));
@@ -33,7 +37,13 @@ export default class ActivateCommand extends Command {
             }
         }
 
-        if (queuedMods.length === 0) { return; }
+        if (queuedMods.length === 0) {
+            console.log('All mods are up-to-date.');
+
+            return;
+        } else {
+            console.log(`Found updates for ${queuedMods.map(mod => mod.name).join(', ')}`);
+        }
 
         // Run SteamCmd
         await SteamCmd.download(...queuedMods.map(mod => mod.id));
@@ -47,6 +57,8 @@ export default class ActivateCommand extends Command {
 
             configMods[index] = mod;
         }
+
+        console.log(`Updated ${updatedMods.map(mod => mod.name).join(', ')}`);
 
         Config.set('mods', configMods);
     }
