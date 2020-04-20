c912352433ce26d68c448e875d188d75f7d23b51
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-04-20 15:58:19 +0300

    Clean up and fix method

--- src/mod.ts
+++ src/mod.ts
@@ -3,23 +3,21 @@ import IMod from './interfaces/iMod';
 import SteamApi from './steam/steamApi';
 
 export default class Mod {
-    public static async getModFromApi(id: number, type: EModType): Promise<IMod> {
-        return (await this.getModsFromApi({ id, type }))[0];
-    }
-
-    public static async getModsFromApi(...mods: { id: number, type: EModType }[]): Promise<IMod[]> {
+    public static async generateModsFromApi(mods: { id: number, steamId: number, type: EModType }[]): Promise<IMod[]> {
         if (mods.length === 0) { return []; }
 
         const processed: IMod[] = [];
 
-        const items = await SteamApi.getPublishedItems(...mods.map(mod => mod.id));
+        const items = await SteamApi.getPublishedItems(...mods.map(mod => mod.steamId));
 
         for (const [index, mod] of mods.entries()) {
             const name = items[index].title;
 
             processed.push({
                 id: mod.id,
+                steamId: mod.steamId,
                 isActive: true,
+                isLocal: false,
                 name,
                 type: mod.type,
             });
