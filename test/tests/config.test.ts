d2c384c67ad22a4988f4025ad1a307f2e7527c79
Ralfs Garkaklis <ralfs@garkaklis.com>
2020-06-08 12:58:19 +0300

    Fully tested config class

--- test/tests/config.test.ts
+++ test/tests/config.test.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs-extra';
 import * as os from 'os';
 import * as path from 'path';
-
 import Config from '../../src/config';
 import NotInitializedError from '../../src/errors/notInitializedError';
+import OutdatedConfigurationFileError from '../../src/errors/outdatedConfigurationFileError';
+
 
 describe('Config.get()', () => {
     test('Returns the correct key from JSON', () => {
@@ -71,5 +72,79 @@ describe('Config.ensureIsInitialized()', () => {
         mock.mockReturnValue(true);
 
         expect(() => Config.ensureIsInitialized()).not.toThrow();
+    });
+});
+
+describe('Config.getLatestVersion()', () => {
+    test('It gets the latest version from package.json', () => {
+        const result = Config.getLatestVersion();
+
+        expect(result).not.toBeUndefined();
+        expect(result).not.toBeNaN();
+        expect(result).toBeGreaterThan(0);
+    });
+});
+
+describe('Config.getCurrentVersion()', () => {
+    test('It returns the correct config version', () => {
+        const orgEnsure = Config.ensureIsInitialized;
+        const orgAll = Config.getAll;
+
+        Config.ensureIsInitialized = jest.fn().mockReturnValue(true);
+
+        Config.getAll = jest.fn().mockReturnValue({
+            version: 2,
+            mods: [],
+        });
+
+        expect(Config.getCurrentVersion()).toBe(2);
+
+        Config.ensureIsInitialized = orgEnsure;
+        Config.getAll = orgAll;
+    });
+
+    test('It returns undefined if the config has no version', () => {
+        const orgEnsure = Config.ensureIsInitialized;
+        const orgAll = Config.getAll;
+
+        Config.ensureIsInitialized = jest.fn().mockReturnValue(true);
+
+        Config.getAll = jest.fn().mockReturnValue({
+            mods: [],
+        });
+
+        expect(Config.getCurrentVersion()).toBeUndefined();
+
+        Config.ensureIsInitialized = orgEnsure;
+        Config.getAll = orgAll;
+    });
+});
+
+describe('Config.ensureIsLatestVersion()', () => {
+    const orgCurrent = Config.getCurrentVersion;
+    const orgLatest = Config.getLatestVersion;
+
+    test('It will throw error if the versions do not match', () => {
+        Config.getCurrentVersion = jest.fn().mockReturnValue(undefined);
+        Config.getLatestVersion = jest.fn().mockReturnValue(2);
+
+        expect(() => Config.ensureIsLatestVersion()).toThrowError(
+            new OutdatedConfigurationFileError(),
+        );
+
+        Config.getCurrentVersion = orgCurrent;
+        Config.getLatestVersion = orgLatest;
+    });
+
+    test('If versions match it will not throw error', () => {
+        Config.getCurrentVersion = jest.fn().mockReturnValue(2);
+        Config.getLatestVersion = jest.fn().mockReturnValue(2);
+
+        expect(() => Config.ensureIsLatestVersion()).not.toThrowError(
+            new OutdatedConfigurationFileError(),
+        );
+
+        Config.getCurrentVersion = orgCurrent;
+        Config.getLatestVersion = orgLatest;
     });
 });
